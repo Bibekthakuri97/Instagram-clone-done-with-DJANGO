fc74891558af11aae6413e269ed7f5955dc20b34
Bibek Shah <bibek_shah01@hotmail.com>
2019-12-22 09:09:51 +0545

    after comments show and profile page

--- photo_app/urls.py
+++ photo_app/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path,include
-from photo_app.views import index,add,edit
+from photo_app.views import index,add,edit,profile,delete
 
 app_name = 'photo_app'
 
 urlpatterns = [
     path('',index,name='index'),
     path('add',add,name='add'),
-    path('edit/<int:id>/',edit,name='edit')
+    path('edit/<int:id>/',edit,name='edit'),
+    path('profile',profile,name='profile'),
+    path('delete/<int:id>/',delete,name='delete'),
 ]
--- photo_app/views.py
+++ photo_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Photomodel,Commentmodel
+from user_app.models import UserModel
 from .forms import Photoform,Commentform
 # Create your views here.
 def index(request):
@@ -59,3 +60,30 @@ def edit(request,id):
         form = Photoform
         return render(request,'photo_app/editphoto.html',{'photo':photo})
  
+def profile(request):
+    if 'id' in request.session:
+        user_id = request.session['id']
+        photos = Photomodel.objects.filter(User = user_id)
+        user = UserModel.objects.filter(id = user_id)
+        dict = {'upload':photos,'user':user}
+        # data = []
+        # for photo in photos:
+        #     two_comments = Commentmodel.objects.filter(parent_post=photo)[:2]
+        #     d = {
+        #         'photo': photo,
+        #         'comments': two_comments
+        #     }
+        #     data.append(d)
+        # context = {
+        #     'posts': data
+        # }
+        # return render(request, 'photo_app/index.html', context)
+        return render(request, 'photo_app/profile.html',dict)
+
+    else:
+        return redirect('user:login')
+    
+def delete(request,id):
+    photo = Photomodel.objects.get(id=id)
+    photo.delete()
+    return redirect('photo_app:profile')
